86fda44fb21e4aafca6de0651668961ee9ed99bd
bstarkman <45607152+bstarkman@users.noreply.github.com>
2018-12-07 20:34:17 -0500

    Initial Commit

---
+++ TP2616J-Blue/src/autonomous.c
@@ -0,0 +1,198 @@
+#include "main.h"
+
+/**
+ * Runs the user autonomous code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the autonomous
+ * mode. Alternatively, this function may be called in initialize or opcontrol
+ * for non-competition testing purposes.
+ *
+ * If the robot is disabled or communications is lost, the autonomous task
+ * will be stopped. Re-enabling the robot will restart the task, not re-start it
+ * from where it left off.
+ */
+
+
+
+#define KF 0
+#define KP 1.0f
+#define KI 0.001f
+#define KD 0.1f
+
+#define turn 605
+#define forward1 1250
+#define backward1 1200
+#define forward2 365
+#define forward3 350
+#define turnf 60
+#define forward4 50
+
+void set_motors(int speed) {
+  motor_move(MOTOR_DRIVE_FRONT_LEFT,speed);
+  motor_move(MOTOR_DRIVE_FRONT_RIGHT,speed);
+  motor_move(MOTOR_DRIVE_BACK_RIGHT,speed);
+  motor_move(MOTOR_DRIVE_BACK_LEFT,speed);
+}
+
+
+
+// Set motor to speed based on distance from `ticks`
+void motor_move_p(int motor, int ticks, float p) {
+    set_motors(p);
+}
+
+// Move to position `ticks` using p for speed
+void wait_motor_move_p(int motor, int ticks, float p) {
+  while ((ticks-motor_get_position(motor)) > 10) { // 10 = threshold, change to change where stop
+    motor_move_p(motor, ticks, p);
+    delay(25);
+  }
+}
+
+void wait_motor_move_p_ac(int motor, int ticks, float p, int actime) {
+  int st = millis();
+  int  ft = st + actime;
+  while ((ticks-motor_get_position(motor)) > 10) { // 10 = threshold, change to change where stop
+    motor_move_p(motor, ticks, (millis() > ft )? p : (p<0)?-50:50 );
+
+    delay(20);
+  }
+}
+
+void wait_motor_move_ac(int motor, int ticks, float p, int actime) {
+  int st = millis();
+  int  ft = st + actime;
+  int sp = (int)motor_get_position(motor);
+      printf("ticks - %d pos - %d sp - %d\n\r",ticks,(int)motor_get_position(motor),sp);
+  while ((abs(ticks)-abs(((int)motor_get_position(motor))-sp)) > 10) { // 10 = threshold, change to change where stop
+    motor_move_p(motor, ticks, (millis() > ft )? p :  (p<0)?-50:50  );
+      printf("ticks - %d pos - %d sp - %d\n\r",ticks,(int)motor_get_position(motor),sp);
+    delay(20);
+  }
+}
+
+
+// Move `ticks` forward from the current position using p for speed
+void wait_motor_move_p_rel(int motor, int ticks, float p) {
+
+  wait_motor_move_p(motor, motor_get_position(motor) + ticks, p);
+}
+
+void wait_motor_move_rel_ac(int motor, int ticks, float p, int actime) {
+
+  wait_motor_move_p_ac(motor, ticks, p, actime);
+}
+
+// Move `ticks` forward from the current position using p for speed
+void flywheel_go(float speed);
+
+
+void autonomous() {
+  double posit, dest;
+  motor_set_gearing(MOTOR_DRIVE_FRONT_LEFT, E_MOTOR_GEARSET_18);
+	motor_set_gearing(MOTOR_DRIVE_BACK_LEFT, E_MOTOR_GEARSET_18);
+	motor_set_gearing(MOTOR_DRIVE_FRONT_RIGHT, E_MOTOR_GEARSET_18);
+	motor_set_gearing(MOTOR_DRIVE_BACK_RIGHT, E_MOTOR_GEARSET_18);
+	motor_set_gearing(MOTOR_DESCORER, E_MOTOR_GEARSET_18);
+	motor_set_gearing(MOTOR_INTAKE, E_MOTOR_GEARSET_18);
+  motor_set_gearing(MOTOR_INDEXER, E_MOTOR_GEARSET_18);
+	motor_set_gearing(MOTOR_FLYWHEEL, E_MOTOR_GEARSET_36);
+  //motor_set_brake_mode(10, E_MOTOR_BRAKE_COAST);
+  //motor_set_brake_mode(4, E_MOTOR_BRAKE_COAST);
+  //motor_set_brake_mode(8, E_MOTOR_BRAKE_COAST);
+  //motor_set_brake_mode(2, E_MOTOR_BRAKE_COAST);
+  motor_set_reversed(10, 1);
+  motor_set_reversed(8, 1);
+
+   motor_move(MOTOR_FLYWHEEL, 127); // flywheel starts
+   motor_move(MOTOR_INTAKE, 127);//intake starts
+
+  posit = motor_get_position(10);
+  dest = posit + 9000.0;
+  printf("start motors\r\n");
+
+
+
+  wait_motor_move_ac(10, forward1, 127, 200);
+
+  set_motors(0);
+  delay(1000);
+
+  wait_motor_move_ac(10, -backward1, -127, 200);
+  set_motors(0);
+
+  delay(1000);
+  motor_tare_position(10);
+  motor_move_relative(10, -turn, -127);
+  motor_move_relative(4, turn, 127);
+  motor_move_relative(8, -turn, -127);
+  motor_move_relative(2, turn, 127);
+  while(motor_get_target_position(10) > motor_get_position(10))
+{
+  printf("turn - %f - %f\r\n",motor_get_target_position(10),motor_get_position(10));
+  delay(10);
+}
+delay(1000);
+
+// wait_motor_move_ac(10, 650, 127, 100);
+// motor_move_relative(10, 600, 127);
+// motor_move_relative(4, 600, 127);
+// motor_move_relative(8, 600, 127);
+// motor_move_relative(2, 600, 127);
+// while(motor_get_target_position(10) > motor_get_position(10))
+// {
+//   delay(3000);
+// }
+//
+printf("turn\r\n");
+motor_tare_position(10);
+wait_motor_move_ac(10, forward2, 127, 200);
+  set_motors(0);
+  printf("shoot\r\n");
+delay(1000);
+ motor_move_relative(MOTOR_INDEXER, 1000, 127);//(MOTOR_INDEXER, 127);
+ delay(1000);
+ motor_tare_position(10);
+ wait_motor_move_ac(10, forward3, 127, 200);
+set_motors(0);
+
+ delay(1000);
+
+ motor_move_relative(MOTOR_INDEXER, 1000, 127);//(MOTOR_INDEXER, 127);
+
+ delay(1000);
+  motor_tare_position(10);
+ motor_move_relative(10, -turnf,-127);
+ motor_move_relative(4,turnf, 127);
+ motor_move_relative(8, -turnf, -127);
+ motor_move_relative(2, turnf, 127);
+ //while(motor_get_target_position(10) > motor_get_position(10))
+// {
+//  printf("turn - %f - %f\r\n",motor_get_target_position(10),motor_get_position(10));
+//  delay(2);
+// }
+//
+
+
+// wait_motor_move_ac(10, 100, 127, 100);
+// motor_move_relative(10, 600, 127);
+// motor_move_relative(4, 600, 127);
+// motor_move_relative(8, 600, 127);
+// motor_move_relative(2, 600, 127);
+// while(motor_get_target_position(10) > motor_get_position(10))
+// {
+//   delay(1000);
+// }
+
+
+//wait_motor_move_ac(10, forward4, 127, 200);
+
+ set_motors(100);
+
+delay(800);
+
+set_motors(0);
+
+motor_move(MOTOR_FLYWHEEL,0);
+
+}
---
+++ TP2616J-RED/src/opcontrol.c
@@ -0,0 +1,151 @@
+#include "main.h"
+#include "2616J.h"
+/**
+ * Runs the operator control code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the operator
+ * control mode.
+ *
+ * If no competition control is connected, this function will run immediately
+ * following initialize()
+ *
+ * If the robot is disabled or communications is lost, the
+ * operator control task will be stopped. Re-enabling the robot will restart the
+ * task, not resume it from where it left off.
+
+ */
+
+
+ #define KF 0
+ #define KP 1.0f
+ #define KI 0.001f
+ #define KD 0.1f
+
+//#define desired 89
+
+ void flywheel_go(float speed){
+  motor_set_gearing(9,E_MOTOR_GEARSET_36);
+  motor_pid_s_t pid = motor_convert_pid(KF, KP, KI, KD);
+  motor_set_vel_pid(9, pid);
+  motor_move_velocity(9,speed);
+}
+
+
+void opcontrol() {
+
+	int left;
+	int right;
+	double driveMult = 1;
+
+	int descorerForward;
+	int descorerBackward;
+
+	int intakeForward;
+	int intakeBackward;
+
+	bool indexerForward;
+	bool indexerBackward;
+
+	bool flywheelIncrease;
+	bool flywheelDecrease;
+
+  //bool atSpeed;
+
+	int count = 0;
+
+	char rpm[10] = "";
+
+
+// if(motor_get_actual_velocity(9) >= desired && !atSpeed)
+//
+// if(atSpeed == 1)
+// {controller_rumble(CONTROLLER_MASTER, "-.-");}
+
+
+	motor_set_gearing(MOTOR_DRIVE_FRONT_LEFT, E_MOTOR_GEARSET_18);
+	motor_set_gearing(MOTOR_DRIVE_BACK_LEFT, E_MOTOR_GEARSET_18);
+	motor_set_gearing(MOTOR_DRIVE_FRONT_RIGHT, E_MOTOR_GEARSET_18);
+	motor_set_gearing(MOTOR_DRIVE_BACK_RIGHT, E_MOTOR_GEARSET_18);
+	motor_set_gearing(MOTOR_DESCORER, E_MOTOR_GEARSET_18);
+	motor_set_gearing(MOTOR_INTAKE, E_MOTOR_GEARSET_18);
+  motor_set_gearing(MOTOR_INDEXER, E_MOTOR_GEARSET_18);
+	motor_set_gearing(MOTOR_FLYWHEEL, E_MOTOR_GEARSET_36);
+
+  motor_set_reversed(8,0);
+  motor_set_reversed(10,0);
+ 		flywheel_go(90);
+	while(true) {
+
+		left = controller_get_analog(E_CONTROLLER_MASTER, E_CONTROLLER_ANALOG_LEFT_Y);
+		right = controller_get_analog(E_CONTROLLER_MASTER, E_CONTROLLER_ANALOG_RIGHT_Y);
+
+		intakeForward = controller_get_digital(E_CONTROLLER_MASTER, E_CONTROLLER_DIGITAL_R1);
+		intakeBackward = controller_get_digital(E_CONTROLLER_MASTER, E_CONTROLLER_DIGITAL_R2);
+
+    indexerForward = controller_get_digital(E_CONTROLLER_MASTER, E_CONTROLLER_DIGITAL_L1);
+    indexerBackward = controller_get_digital(E_CONTROLLER_MASTER, E_CONTROLLER_DIGITAL_L2);
+
+		descorerForward = controller_get_digital(E_CONTROLLER_MASTER, E_CONTROLLER_DIGITAL_B);
+    descorerBackward = controller_get_digital(E_CONTROLLER_MASTER, E_CONTROLLER_DIGITAL_A);
+
+		flywheelIncrease = controller_get_digital(E_CONTROLLER_MASTER, E_CONTROLLER_DIGITAL_UP);
+		flywheelDecrease = controller_get_digital(E_CONTROLLER_MASTER, E_CONTROLLER_DIGITAL_DOWN);
+
+		if(controller_get_digital_new_press(E_CONTROLLER_MASTER, E_CONTROLLER_DIGITAL_X)) {
+			driveMult = 1;
+		} else if(controller_get_digital_new_press(E_CONTROLLER_MASTER, E_CONTROLLER_DIGITAL_Y)) {
+			driveMult = 0.7;
+			//change low drive speed by changing (0.5) to something different
+		}
+      // sets the speed of the motors and how they should function
+		motor_move(MOTOR_DRIVE_FRONT_LEFT, left * driveMult);
+		motor_move(MOTOR_DRIVE_FRONT_RIGHT, -1 * right * driveMult);
+		motor_move(MOTOR_DRIVE_BACK_LEFT, left * driveMult);
+		motor_move(MOTOR_DRIVE_BACK_RIGHT, -1 * right * driveMult);
+
+		if (descorerForward == 1)
+			motor_move(MOTOR_DESCORER, descorerForward * 127);
+		else if (descorerBackward == 1)
+			motor_move(MOTOR_DESCORER, descorerBackward * -127);
+		else
+			motor_move(MOTOR_DESCORER, 0);
+
+		if (intakeForward == 1)
+			motor_move(MOTOR_INTAKE, intakeForward * 127);
+		else if (intakeBackward == 1)
+			motor_move(MOTOR_INTAKE, intakeBackward * -127);
+		else
+			motor_move(MOTOR_INTAKE, 0);
+
+		if (indexerForward == 1)
+			motor_move(MOTOR_INDEXER, indexerForward * 100);
+		else if (indexerBackward == 1)
+			motor_move(MOTOR_INDEXER, indexerBackward * -100);
+		else
+			motor_move(MOTOR_INDEXER, 0);
+       /**
+			 * sets the speed for the flywheel and makes it so once the button
+			 * is pressed the speed will stay at 127 and when the other button is
+			 * pressed the speed will go down to zero
+			 */
+		if (flywheelIncrease == 1)
+		flywheel_go(90);
+		  // motor_move(MOTOR_FLYWHEEL, flywheelIncrease * 127);
+		else if (flywheelDecrease == 1)
+		   motor_move(MOTOR_FLYWHEEL, flywheelDecrease * 0);
+
+
+			if (!(count % 50)) {
+	      // Only print every 50ms, the controller text update rate is slow
+				sprintf(rpm, "RPM: %.2f    ", motor_get_actual_velocity(9));
+				printf("%s\n",rpm);
+				controller_print(E_CONTROLLER_MASTER, 0, 0, rpm);
+				delay(0);
+			  controller_clear_line(E_CONTROLLER_MASTER, 0);
+	    }
+	    count++;
+	    delay(10);
+
+   }
+
+}
